8ae90f71093986f7723f0c2ad1f87d339652362e
Tomás Francisco <mail@tomasfrancisco.com>
2014-04-27 00:28:20 +0100

    GUI implemented
    Necessário validar os resultados

--- Aleatorio.java
+++ Aleatorio.java
@@ -5,7 +5,7 @@ package sccsimulator;
 
 public class Aleatorio {
 
-	// Gera um n�mero segundo uma distribuição exponencial negativa de m�dia m
+	// Gera um número segundo uma distribuição exponencial negativa de m�dia m
     static double exponencial (double m, int seed){
 		return (-m*Math.log(RandomGenerator.rand64(seed)));
 	}
